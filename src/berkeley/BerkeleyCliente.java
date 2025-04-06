9b71a640adc67bc6946522f9d31f4f3303d46b4d
LucasQReis <lqrreis@furb.br>
2025-04-06 20:05:05 -0300

    feat: passando nome como argumento

--- src/berkeley/BerkeleyCliente.java
+++ src/berkeley/BerkeleyCliente.java
@@ -4,20 +4,29 @@ import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Date;
+import java.util.Scanner;
 
 public class BerkeleyCliente extends UnicastRemoteObject implements BerkeleyInterface {
     private Date horarioLocal;
     private final String nome;
 
     public static void main(String[] args) throws Exception {
-        BerkeleyCliente cliente = new BerkeleyCliente(args[0]);
-        Naming.rebind(args[0], cliente);
+        try {
+            BerkeleyCliente cliente = new BerkeleyCliente();
+            Naming.rebind(cliente.nome, cliente);
+        } catch (RemoteException e) {
+            throw new IllegalArgumentException("Informe o nome do cliente: " + e.getMessage());
+        }
     }
 
-    protected BerkeleyCliente(String nome) throws RemoteException {
-        this.nome = nome;
-        this.horarioLocal = new Date(System.currentTimeMillis() + (long) (Math.random() * 120000 - 60000));
-        System.out.println(nome + " iniciado com horário: " + horarioLocal);
+    protected BerkeleyCliente() throws RemoteException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do cliente: ");
+        this.nome = scanner.nextLine();
+        if (!nome.isEmpty()) {
+            this.horarioLocal = new Date(System.currentTimeMillis() + (long) (Math.random() * 120000 - 60000));
+            System.out.println(nome + " iniciado com horário: " + horarioLocal);
+        }
     }
 
     @Override
